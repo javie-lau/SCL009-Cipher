225f41015e987cd83095df1e96bdd48701798377
lau <laura.javiera77@gmail.com>
2019-04-04 11:50:19 -0300

    cipher.js listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,65 @@
 window.cipher = {
-  encode: () => {
+  encode: (string, offset) => {
     
+    let cifradoFinal="";
+
+    for (let i = 0; i < string.length; i++) {
+        let texto = string[i];
+        //console.log(texto);
+        let text = "";
+        let caracter = texto.charCodeAt();
+        //console.log(caracter);
+        if (caracter>= 65 && caracter <= 90){
+        text = String.fromCharCode(((caracter - 65 + offset) % 26) + 65);
+       // console.log(text);
+        } 
+        
+        else{
+           alert("Solo letras de la A a la Z");return;
+                   
+          
+            
+            
+        }
+
+             cifradoFinal += text;
+            // console.log(cifradoFinal);
+            
+    }
+    return cifradoFinal  
+    
+
       
        
    
     
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (string, offset) => {
+    let decifradoFinal="";
+
+    for (let i = 0; i < string.length; i++) {
+        let texto = string[i];
+        //console.log(texto);
+        let text = "";
+        let caracter = texto.charCodeAt();
+        //console.log(caracter);
+        if (caracter>= 65 && caracter <= 90){
+        text = String.fromCharCode(((caracter + 65 - offset) % 26) + 65);
+       // console.log(text);
+        }
+        else{
+            alert("Solo letras de la A a la Z");return;
+                        
+             
+             
+             
+         }
+
+             decifradoFinal += text;
+            // console.log(cifradoFinal);
+            
+
+    }
+    return decifradoFinal  
   }
 };
--- src/index.js
+++ src/index.js
@@ -11,74 +11,21 @@ document.getElementById("comenzar").addEventListener("click",() => {
 
 
 
-document.getElementById("botonCifrado").addEventListener("click", () => {
+document.getElementById("botonCifrado").addEventListener("click",() => {
     let string = document.getElementById("escribir").value.toUpperCase();
     let offset=parseInt(document.getElementById("offset").value);
     //console.log(offset);
-
-
-    let cifradoFinal="";
-
-    for (let i = 0; i < string.length; i++) {
-        let texto = string[i];
-        //console.log(texto);
-        let text = "";
-        let caracter = texto.charCodeAt();
-        //console.log(caracter);
-        if (caracter>= 65 && caracter <= 90){
-        text = String.fromCharCode(((caracter - 65 + offset) % 26) + 65);
-       // console.log(text);
-        } 
-        
-        else{
-           alert("Solo letras de la A a la Z");return;
-                       
-            
-            
-            
-        }
-
-             cifradoFinal += text;
-            // console.log(cifradoFinal);
-            document.getElementById("respuesta").value =cifradoFinal;
-
-    }
-    return cifradoFinal  
-    
-
+let nuevaVariable = cipher.encode(string,offset);//importe la funcion encode de cipher.js, 
+document.getElementById("respuesta").value =nuevaVariable;
 });
-document.getElementById("decifrando").addEventListener("click", () => {
+
+document.getElementById("decifrando").addEventListener("click",() => {
 
  let string = document.getElementById("escribir").value.toUpperCase();
  let offset=parseInt(document.getElementById("offset").value);
     //console.log(offset);
 
-
-    let decifradoFinal="";
-
-    for (let i = 0; i < string.length; i++) {
-        let texto = string[i];
-        //console.log(texto);
-        let text = "";
-        let caracter = texto.charCodeAt();
-        //console.log(caracter);
-        if (caracter>= 65 && caracter <= 90){
-        text = String.fromCharCode(((caracter + 65 - offset) % 26) + 65);
-       // console.log(text);
-        }
-        else{
-            alert("Solo letras de la A a la Z");return;
-                        
-             
-             
-             
-         }
-
-             decifradoFinal += text;
-            // console.log(cifradoFinal);
-            document.getElementById("respuesta").value = decifradoFinal;
-
-    }
-    return decifradoFinal  
+    let nuevaVariable = cipher.decode(string,offset);
+    document.getElementById("respuesta").value = nuevaVariable;
     
 });
