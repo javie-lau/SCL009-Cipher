d76992a22a64cc2f3d14114ae9cdef9ed4ca6044
lau <laura.javiera77@gmail.com>
2019-04-07 21:23:34 -0400

    offset negativo

--- src/cipher.js
+++ src/cipher.js
@@ -8,25 +8,35 @@ window.cipher = {
         //console.log(texto);
         let text = "";
         let caracter = texto.charCodeAt();
-        //console.log(caracter);
-        if (caracter>= 65 && caracter <= 90){
-        text = String.fromCharCode(((caracter - 65 + offset) % 26) + 65);
-       // console.log(text);
-        } 
-        else if (caracter>=97 && caracter<=122){
-          text = String.fromCharCode(((caracter - 97 + offset) % 26) + 97);
-        }
-        else if (caracter==32){
-          text = String.fromCharCode(caracter);
-        }
-        
-        else{
-           alert("Solo puedes ingresar letras");return;
-                   
-        }
+        let otherCharacters = (caracter>=32 && caracter<=47)||(caracter>=58 && caracter<=64)||
+        (caracter>=91 && caracter<=96)||(caracter>=123 && caracter<=254);
 
-             cifradoFinal += text;
-            // console.log(cifradoFinal);
+        //offset positivo
+        if(offset>=0){ if (caracter>= 65 && caracter <= 90){
+        text = String.fromCharCode(((caracter - 65 + offset) % 26) + 65);//mayuscula
+               } 
+        else if (caracter>=97 && caracter<=122){
+          text = String.fromCharCode(((caracter - 97 + offset) %26) + 97);//minuscula
+        } if(caracter>=48 && caracter<=57){ 
+          text= String.fromCharCode((caracter - 48+ offset) %10 + 48);//numeros
+        } if(otherCharacters){ 
+          text = String.fromCharCode(caracter);//otros caracteres   
+        }
+      }//offset negativo
+      if(offset<0){ if(caracter>=65 && caracter<=90){ 
+        text = String.fromCharCode((caracter - 90 + offset) %26 + 90);//mayuscula    
+      } if(caracter>=97 && caracter<=122){ 
+        text = String.fromCharCode((caracter - 122 + offset)%26 + 122);
+      }//minusculas
+        if(caracter>=48 && caracter<=57){
+        text = String.fromCharCode((caracter - 57 + offset) %10 + 57);//numeros
+      } if(otherCharacters){ 
+        text = String.fromCharCode(caracter);//otros caracteres 
+      }
+    }
+                
+                     cifradoFinal += text;
+          
             
     }
     return cifradoFinal  
@@ -41,25 +51,38 @@ window.cipher = {
      
         let text = "";
         let caracter = texto.charCodeAt();
-      
-        if (caracter>= 65 && caracter <= 90){
-        text = String.fromCharCode(((caracter + 65 - offset) % 26) + 65);
+        let otherCharacters = (caracter>=32 && caracter<=47)||(caracter>=58 && caracter<=64)||
+        (caracter>=91 && caracter<=96)||(caracter>=123 && caracter<=254);
+
+      // offset positivo
+        if(offset>=0){ if (caracter>= 65 && caracter <= 90){
+        text = String.fromCharCode(((caracter + 65 - offset) % 26) + 65);//mayuscula
       
         }
-        else if (caracter>=97 && caracter<=122){ //caracter>=97 && caracter<=122
-          text = String.fromCharCode(((caracter -122 - offset) % 26) + 122); //caracter + 97 - offset) % 26) + 97
-        }
-        else if (caracter==32){
+         if (caracter>=97 && caracter<=122){ 
+          text = String.fromCharCode(((caracter -122 - offset) % 26) + 122); //minusculas
+         }if(caracter>=48 && caracter<=57){ 
+            text= String.fromCharCode((caracter- 57- offset) % 10 + 57);//numeros
+         } if(otherCharacters){ 
           text = String.fromCharCode(caracter);
         }
-
-        else{
-            alert("Solo letras de la A a la Z");return;
-                        
+      }
+//offset negativo
+                
+       if (offset<0){ if (caracter>= 65 && caracter <= 90){
+        text = String.fromCharCode(((caracter - 65 - offset) % 26) + 65);//mayuscula
+      
+        }
+         if (caracter>=97 && caracter<=122){ 
+          text = String.fromCharCode(((caracter - 97 - offset) % 26) + 97); //minusculas
+         }if(caracter>=48 && caracter<=57){ 
+            text= String.fromCharCode((caracter- 48- offset) % 10 + 48);//numeros
+         } if(otherCharacters){ 
+          text = String.fromCharCode(caracter);
+        }
+      }    
              
-           
-             
-         }
+         
 
              decifradoFinal += text;
             // console.log(cifradoFinal);
